ba57354e86044b89b37ceeed25ae3e767081230c
tochman <thomas@nocebo.se>
2016-03-17 15:35:47 +0100

    WIP new layout

--- app/assets/javascripts/mobile/mobile.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require jquery.mobile
-//= require_directory .
-//= require turbolinks
-
-$(document).on("page:fetch", startSpinner);
-$(document).on("page:receive", stopSpinner);
